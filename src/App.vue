32bf2866799d68f7cab64df444d6b0de1275157c
vigneshwaran k <vigneshwarank@vigneshwarans-MacBook-Air.local>
2023-06-03 12:09:38 +0530

    add chip component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ChipButton :chip="'News'" :isSelected="true" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ChipButton from "./components/ChipButton.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ChipButton,
+  },
+  data() {
+    return {
+      chipList: [
+        "All",
+        "News",
+        "Music",
+        "England cricket team",
+        "Wickets",
+        "Live",
+      ],
+    };
   },
 };
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Roboto", "Arial", sans-serif;
+  background: #0f0f0f;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/ChipButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="chip">
+    <div :class="{ 'chip-selected': isSelected }">
+      {{ chip }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChipButton",
+  props: {
+    chip: String,
+    isSelected: Boolean,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "~rfs/scss";
+.chip {
+  display: flex;
+  gap: 12px;
+  div {
+    @include padding(12px);
+    background: #ffffff1a;
+    color: #f1f1f1;
+    height: 32px;
+    min-width: 12px;
+    padding: 0 12px;
+    border-radius: 8px;
+    box-sizing: border-box;
+    outline: none;
+    overflow: hidden;
+    cursor: pointer;
+    @include font-size(14px);
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    &.chip-selected {
+      color: #0f0f0f;
+      background: #f1f1f1;
+    }
+  }
+}
+</style>
